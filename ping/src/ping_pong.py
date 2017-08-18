56079cd92cc20c48e7236e53eb1688cbb59f69b0
Eduardo Patrocinio <epatro@gmail.com>
2017-08-18 14:59:40 -0300

    Build chart

--- ping/src/ping_pong.py
+++ ping/src/ping_pong.py
@@ -9,49 +9,32 @@ import sys
 
 app = Flask(__name__)
 
-channel_name = "stocks"
+channel_name = "ping"
 
 base_url = "http://stock_price:8080/stockPrice/"
 
 set_url = "http://stock_table_set:8080/"
 
-def setStockPrice(js):
-	stock = js['symbol']
-	price = js['price']
-	pe = js['pe']
-
-	print("Calling setStockPrice with stock: ", 
-		stock, " price: ", price, " pe: ", pe)
-
-	sys.stdout.flush()
-	url = set_url + stock
-	payload = { "price": price, "pe": pe }
-
-	print("url: ", url, " payload: ", payload)
-	resp = requests.post(url, data = payload)
-	status = resp.status_code
-	if status == 200 or status == 304:
-		print('status code: ', status)
-	else:
-		print('Error status code: ', status)
+def bounceBall(js):
+	print ("Bouncindg ball %s" % js)
 
 def callback(ch, method, properties, body):
 	symbol = str(body, 'utf-8')
 	url = base_url + symbol
-	print("Received stock %r" % body)
+	print("Received ball %r" % body)
 	resp = requests.get(url)
 	status = resp.status_code
 	if status == 200 or status == 304:
 		print('status code: ', status)
 		js = resp.json()
 		print("Received response %r" % js)
-		setStockPrice(js)
+		bounceBall(js)
 	else:
 		print('Error status code: ', status)
 	sys.stdout.flush()
 
 def consume():
-	print('Consuming stocks')
+	print('Consuming balls')
 	host = os.getenv('QUEUE_HOST', 'localhost')
 	print(host)
 	connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
@@ -66,21 +49,21 @@ def consume():
 	channel.start_consuming()
 
 
-def consumeStocks():
+def consumeBalls():
 	print("Starting a thread")
 	t = threading.Thread(target=consume)
 	t.start()
 
 @app.route("/process")
-def processStocks():
-	consumeStocks()
+def processBalls():
+	consumeBalls()
 	return 'process'
 
 @app.route('/')
 def index():
     return 'Index Page'
 
-print ("Welcome to process stock")
+print ("Welcome to Ping Pong")
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
 
