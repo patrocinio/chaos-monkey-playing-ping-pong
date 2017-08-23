987f736a041cb4f2ec0f614f400fc7f22d2d0665
Eduardo Patrocinio <epatro@gmail.com>
2017-08-23 15:13:33 -0300

    Working on throwing balls

--- Sets/Set-1/throw_ball/build.sh
+++ Sets/Set-1/throw_ball/build.sh
@@ -1,7 +1,7 @@
 export IMAGE=ping_pong_throw_ball:latest
-export ICP_DTR=9.19.34.109:8500
+export KUBE_DTR=169.57.182.101:8500
 export IMAGE_PATH=$ICP_DTR/ping-pong/$IMAGE
 
 docker build -t $IMAGE_PATH .
-docker login $ICP_DTR
+docker login $KUBE_DTR
 docker push $IMAGE_PATH
