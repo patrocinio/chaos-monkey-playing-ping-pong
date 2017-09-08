fb3da454892bbc8c971cc1eaf2a0c2a5b9365599
Eduardo Patrocinio <epatro@gmail.com>
2017-09-08 14:26:03 -0400

    Set create_namespace.sh

--- scripts/create_namespace.sh
+++ scripts/create_namespace.sh
@@ -1,4 +1,8 @@
-CONTEXT=minikube
+#CONTEXT=minikube
+#USER=minikube
+
+CONTEXT=cfc
+USER=cfcadmin
 
 kubectl create namespace ping-pong
-kubectl config set-context $CONTEXT --user=minikube --namespace=ping-pong
+kubectl config set-context $CONTEXT --user=$USER --namespace=ping-pong
