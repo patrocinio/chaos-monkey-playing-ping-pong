5097d41317e6b1607830eb697dd9ea04b450defd
Eduardo Patrocinio <epatro@gmail.com>
2018-05-15 17:12:23 -0300

    Redefine Game 1

--- v2/Game-1/src/ping_pong/src/ping_pong.py
+++ v2/Game-1/src/ping_pong/src/ping_pong.py
@@ -3,25 +3,16 @@ import sys
 import message
 import os
 import time
-import redis
-
-CACHE='ping-pong-redis-ha-master-svc.ping-pong.svc.cluster.local'
-#CACHE='localhost'
 
 
-queue_name = os.environ['QUEUE_NAME']
+queue_name = os.environ['QUEUE_NAME']    
 other_queue = os.environ['OTHER_QUEUE_NAME']
 
 key = queue_name
 
 welcome_message = "==> I'm " +queue_name + " in v2/Game-1"
 
-red = 0
-
-def throw_ball(ball):
-	message.send ("Deleting key " + key)
-	red.delete(key)
-
+def throwBall(ball):
 	message.send ("==> Throwing ball %s" % ball)
 
 	channel.basic_publish(exchange='',
@@ -34,42 +25,18 @@ def transformBall(body):
 	count += 1
 	return name + "-" + str(count)
 
-def checkCache():
-	value = red.get(key)
-	if value is None:
-		print ("Nothing in the cache")
-		message.send ("Nothing found in the cache")
-	else:
-		# Cleans the cache
-		print ("Found a ball %r " % value)
-		message.send ("Found a ball %r " % value)
-
-		ball = transformBall(value)
-
-		throw_ball(ball)
-
 def callback(ch, method, properties, body):
 	print ("Received a message %s" % body)
 	sys.stdout.flush ()
 	message.send ("I received a %s ball" % body)
 
-	# Store in cache
-	message.send ("Storing key " + key)
-	red.set(key, body)
-
 	# Sleep a second
 	time.sleep(1)
 
 	# Throw the ball
-	# throw_ball(ball)
-	checkCache()
-
-def initCache():
-	print ("Initializing cache")
-	global red
-#	red = redis.StrictRedis(host=CACHE, password='ping_pong')
-	red = redis.StrictRedis(host=CACHE)
-
+	ball = transformBall(body)
+	throwBall(ball)
+  
 print(welcome_message)
 
 # Init message
@@ -82,12 +49,7 @@ connection = pp_queue.connect()
 channel = connection.channel()
 channel.queue_declare(queue=other_queue)
 
-# Init cache
-initCache()
-
-# Check cache
-checkCache()
-
 # Init queue
 pp_queue.consume(queue_name, callback)
+
 
