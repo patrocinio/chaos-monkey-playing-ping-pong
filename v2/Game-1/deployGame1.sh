0f0ce3114cd205178b928c23bc9dbd7fa86bfb81
Eduardo Patrocinio <epatro@gmail.com>
2018-05-15 12:35:21 -0300

    Fix pong

--- v2/Game-1/deployGame1.sh
+++ v2/Game-1/deployGame1.sh
@@ -5,4 +5,6 @@ echo Deleting $NAME
 helm delete $NAME --purge --tls
 
 echo Deploying $NAME
-helm install --name $NAME ./helm/ping-pong --namespace $NAMESPACE --tls
+helm install --name $NAME ./helm/ping-pong --namespace $NAMESPACE --tls 
+
+#--dry-run --debug
