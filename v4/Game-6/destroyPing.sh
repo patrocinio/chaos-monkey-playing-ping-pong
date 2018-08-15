5421c3628b44353e06339d13ac9da35a249a67c2
Eduardo Patrocinio <epatro@gmail.com>
2018-08-15 11:22:35 -0400

    Destroy one Ping Pod only

--- v4/Game-6/destroyPing.sh
+++ v4/Game-6/destroyPing.sh
@@ -1,3 +1,6 @@
-echo Deleting ping  Pod
-kubectl delete pod -l app=ping --grace-period=0 --force
+POD_ID=`kubectl get pod -l app=ping | grep -v NAME | head -1 | awk '{print $1}'`
+
+
+echo Deleting ping  Pod $POD_ID
+kubectl delete pod $POD_ID --grace-period=0 --force
 
